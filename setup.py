d5e17680875b6ac795710076fec68021e513ca8c
Aaron Quinlan <arq5x@virginia.edu>
2012-06-25 14:44:37 -0400

    issue #22: add pybedtools to setup.py

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
         #install_requires=['numpy>=1.6.0', 'pyparsing>=1.5.6', 'pysam>=0.6', 'pyvcf>=0.4.2'],
         # temp. until cyvcf is merged intop PyVcf
         install_requires=['numpy>=1.6.0', 'pyparsing>=1.5.6', 'pysam>=0.6', 'cyvcf>=0.1.1',
-                          'PyYAML >= 3.10'],
+                          'PyYAML >= 3.10', 'pybedtools>=0.6.1'],
         dependency_links = ['http://github.com/arq5x/cyvcf/tarball/master#egg=cyvcf-0.1.1'],
         requires = ['python (>=2.5, <3.0)'],
         packages=['gemini',
