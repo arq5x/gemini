f27710d59829ab4b763c019d3d08814f5c6f2ad6
Aaron Quinlan <arq5x@virginia.edu>
2012-07-25 18:28:14 -0400

    prefer hgnc from vep, yet allow ensembl gene; in support of KEGG pathways

--- gemini/vep.py
+++ gemini/vep.py
@@ -21,7 +21,8 @@ class EffectDetails(object):
         self.codon_change = fields[1] if fields[1] != '' else None
         self.aa_change = fields[2] if fields[2] != '' else None
         self.ensembl_gene = fields[3] if fields[3] != '' else None
-        self.gene = fields[4] if fields[4] != '' else None
+        self.hgnc = fields[4] if fields[4] != '' else None
+        self.gene = self.hgnc if fields[4] != '' else self.ensembl_gene
         self.transcript = fields[5] if fields[5] != '' else None
         self.exon = fields[6] if fields[6] != '' else None
         self.polyphen =  fields[7] if fields[7] != '' else None
