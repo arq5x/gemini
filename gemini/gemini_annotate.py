4e352c5d175decd33463ccf4c54b2f3ab9f7b025
chapmanb <chapmanb@50mail.com>
2012-10-13 06:31:22 -0400

    Separate selection and update database interaction to avoid sqlite3 lock errors. Generalize annotation updates to handle multiple column types through a single worker function.

--- gemini/gemini_annotate.py
+++ gemini/gemini_annotate.py
@@ -24,90 +24,84 @@ def add_requested_column(col_name, update_cursor):
         pass
         #sys.exit("ERROR: column \"" + col_name + "\" already exists in variants table")  
 
+def _annotate_variants(args, conn, get_val_fn):
+    """Generalized annotation of variants with a new column.
+    
+    get_val_fn takes a list of annotations in a region and returns
+    the value for that region to update the database with.
 
-def annotate_variants_bool(args, select_cursor, update_cursor):
+    Separates selection and identification of values from update, to avoid concurrent
+    database access errors from sqlite3, especially on NFS systems. The retained
+    to_update list is small, but batching could help if memory issues emerge.
+    """
+    # For each, use Tabix to detect overlaps with the user-defined
+    # annotation file.  Update the variant row with T/F if overlaps found.
+    annos = pysam.Tabixfile(args.anno_file)
+    naming = guess_contig_naming(annos)
+    select_cursor = conn.cursor()
+    select_cursor.execute("SELECT chrom, start, end, variant_id FROM variants")
+    to_update = []
+    for row in select_cursor:
+        to_update.append((str(row["variant_id"]),
+                          get_val_fn(annotations_in_region(row, annos, "tuple", naming))))
+    update_cursor = conn.cursor()
+    add_requested_column(args.col_name, update_cursor)
+    for variant_id, val in to_update:
+        update_qry = "UPDATE variants SET " + args.col_name + " = " + str(val) + \
+                     " WHERE variant_id = " + variant_id
+        update_cursor.execute(update_qry)
+
+def annotate_variants_bool(args, conn):
     """
     Populate a new, user-defined column in the variants
     table with a BOOLEAN indicating whether or not
     overlaps were detected between the variant and the 
     annotation file.
     """
-    add_requested_column(args.col_name, update_cursor)
-
-    # For each, use Tabix to detect overlaps with the user-defined
-    # annotation file.  Update the variant row with T/F if overlaps found.
-    annos = pysam.Tabixfile(args.anno_file)
-    naming = guess_contig_naming(annos)
-    select_cursor.execute("SELECT chrom, start, end, variant_id FROM variants")
-    for row in select_cursor:
-        has_hit = False
-        for hit in annotations_in_region(row, annos, naming=naming):
-            has_hit = True
+    def has_anno_hit(hits):
+        has_hit = 0
+        for hit in hits:
+            has_hit = 1
             break
-        if has_hit:
-            update_qry = "UPDATE variants SET " + args.col_name + " = 1 " + \
-                         " WHERE variant_id = " + str(row['variant_id'])
-        else:
-            update_qry = "UPDATE variants SET " + args.col_name + " = 0 " + \
-                         " WHERE variant_id = " + str(row['variant_id'])
-        update_cursor.execute(update_qry)
+        return has_hit
+    return _annotate_variants(args, conn, has_anno_hit)
 
-
-def annotate_variants_count(args, select_cursor, update_cursor):
+def annotate_variants_count(args, conn):
     """
     Populate a new, user-defined column in the variants
     table with a INTEGER indicating the count of overlaps
     between the variant and the 
     annotation file.
     """
-    add_requested_column(args.col_name, update_cursor)
-
-    # For each, use Tabix to count overlaps with the user-defined
-    # annotation file.  Update the variant row with the count.
-    annos = pysam.Tabixfile(args.anno_file)
-    naming = guess_contig_naming(annos)
-    select_cursor.execute("SELECT chrom, start, end, variant_id FROM variants")
-    for row in select_cursor:
+    def get_hit_count(hits):
         count = 0
-        for hit in annotations_in_region(row, annos, naming=naming):
+        for hit in hits:
             count += 1
-        update_qry = "UPDATE variants SET " + args.col_name + " = " + str(count) + \
-                     " WHERE variant_id = " + str(row['variant_id'])
-        update_cursor.execute(update_qry)
+        return count
+    return _annotate_variants(args, conn, get_hit_count)
 
-def annotate_variants_list(args, select_cursor, update_cursor):
+def annotate_variants_list(args, conn):
     """
     Populate a new, user-defined column in the variants
     table with a INTEGER indicating the count of overlaps
     between the variant and the 
     annotation file.
     """
-    add_requested_column(args.col_name, update_cursor)
-
-    # For each, use Tabix to count overlaps with the user-defined
-    # annotation file.  Update the variant row with the count.
-    annos = pysam.Tabixfile(args.anno_file)
-    naming = guess_contig_naming(annos)
-    select_cursor.execute("SELECT chrom, start, end, variant_id FROM variants")
-    for row in select_cursor:
+    def get_hit_list(hits):
         hit_list = []
-        for hit in annotations_in_region(row, annos, "tuple", naming=naming):
+        for hit in hits:
             try:
                 hit_list.append(hit[int(args.col_extract) - 1])
             except IndexError:
                 sys.exit("Column " + args.col_extract + " exceeds \
                           the number of columns in your \
                           annotation file. Exiting.")
-                          
-        hits = ",".join(hit_list)
-        if len(hit_list):
-            update_qry = "UPDATE variants SET " + args.col_name + " = '" + hits + \
-                        "' WHERE variant_id = " + str(row['variant_id'])
+        if len(hit_list) > 0:
+            val = ",".join(hit_list)
+            return "'%s'" % val
         else:
-            update_qry = "UPDATE variants SET " + args.col_name + " = NULL" + \
-                        " WHERE variant_id = " + str(row['variant_id'])
-        update_cursor.execute(update_qry)
-
+            return "NULL"
+    return _annotate_variants(args, conn, get_hit_list)
 
 def annotate(parser, args):
 
@@ -125,18 +119,16 @@ def annotate(parser, args):
     conn = sqlite3.connect(args.db)
     conn.row_factory = sqlite3.Row # allow us to refer to columns by name
     conn.isolation_level = None
-    select_cursor = conn.cursor()
-    update_cursor = conn.cursor()
 
     if args.col_type == "boolean":
-        annotate_variants_bool(args, select_cursor, update_cursor)
+        annotate_variants_bool(args, conn)
     elif args.col_type == "count":
-        annotate_variants_count(args, select_cursor, update_cursor)
+        annotate_variants_count(args, conn) 
     elif args.col_type == "list":
         if args.col_extract is None:
             sys.exit("You must specify which column to extract (-e) from the annotation file.")
         else:
-            annotate_variants_list(args, select_cursor, update_cursor)
+            annotate_variants_list(args, conn)
     else:
         sys.exit("Unknown column type requested. Exiting.")
 
