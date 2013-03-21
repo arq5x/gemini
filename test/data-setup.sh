14a1003a370e5fa09d84b7781f050aa9d1b70ed7
Uma Paila <udp3f@d-128-109-228.bootp.virginia.edu>
2013-03-21 15:30:04 -0400

    unit tests added for pathway tool

--- test/data-setup.sh
+++ test/data-setup.sh
@@ -3,3 +3,5 @@ gemini load -v test1.snpeff.vcf -t snpEff test1.snpeff.db
 gemini load -v test2.snpeff.vcf test2.snpeff.db
 gemini load -v test3.snpeff.vcf test3.snpeff.db
 gemini load -v test.clinvar.vcf test.clinvar.db
+gemini load -v test4.vep.snpeff.vcf -t snpEff test4.snpeff.db
+gemini load -v test4.vep.snpeff.vcf -t VEP test4.vep.db
