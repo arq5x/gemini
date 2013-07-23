4658f64124b2e64175f4641895e6f40cd62dec94
arq5x <arq5x@virginia.edu>
2013-07-23 19:45:24 -0400

    [ENH] de_novo; add --filter, --columns, and --gt_filter.; default -d to 0; don't enforce num_het== 1

--- gemini/tool_de_novo_mutations.py
+++ gemini/tool_de_novo_mutations.py
@@ -5,106 +5,95 @@ import sys
 import collections
 from copy import copy
 
-import compression
+import GeminiQuery
 import gemini_utils as util
 from gemini_constants import *
 import gemini_subjects as subjects
 
-
-def get_de_novo_candidates(c, min_sample_depth=30):
+def get_de_novo_candidates_2(args):
     """
     Report candidate variants that meet appear to be de novo
-    mutations in the child. We cannot distinguisj mutations that
+    mutations in the child. We cannot distinguish mutations that
     occured in the parental germline from those that occurred
     early in development in the child post-conception.
     """
+    gq = GeminiQuery.GeminiQuery(args.db, include_gt_cols=True)
+    
+    if args.columns is not None:
+        # the user only wants to report a subset of the columns
+        query = "SELECT " + str(args.columns) + " FROM variants"
+    else:
+        # report the kitchen sink
+        query = "SELECT *" + \
+                ", gts, gt_types, gt_phases, gt_depths, \
+                gt_ref_depths, gt_alt_depths, gt_quals" + \
+                " FROM variants"
 
-    families = subjects.get_families(c)
+    # add any non-genotype column limits to the where clause
+    if args.filter:
+        query += " WHERE " + args.filter
 
+    # collect family info
+    families = subjects.get_families(gq.c)
+    family_ids = []
+    family_masks = []
+    family_sample_gt_labels = []
+    family_sample_gt_columns = []
+    family_sample_depth_columns = []
     for family in families:
+        family_masks.append(family.get_de_novo_filter())
+        family_sample_gt_labels.append(family.get_subject_genotype_labels())
+        family_sample_gt_columns.append(family.get_subject_genotype_columns())
+        family_sample_depth_columns.append(family.get_subject_depth_columns())
+        family_ids.append(family.family_id)
 
-        query = "SELECT chrom, start, end, ref, alt, gene, \
-                        impact, impact_severity, in_dbsnp, \
-                        rs_ids, aaf_1kg_all, aaf_esp_all, \
-                        clinvar_sig, clinvar_disease_name, \
-                        clinvar_dbsource, gt_types, \
-                        gt_depths, gts \
-                 FROM variants \
-                 WHERE impact_severity != 'LOW' \
-                 AND num_het = 1"
+    # run the query applying any genotype filters provided by the user.
+    gq.run(query, args.gt_filter)
 
-        c.execute(query)
-        all_query_cols = [str(tuple[0]) for tuple in c.description
-                          if not tuple[0].startswith("gt")]
+    # print a header
+    print "family_id\tfamily_members",
+    print gq.header
 
-        family_genotype_mask = family.get_de_novo_filter()
-        family_sample_gt_columns = family.get_subject_genotype_columns()
-        family_sample_depth_columns = family.get_subject_depth_columns()
-        family_sample_gt_labels = family.get_subject_genotype_labels()
-        family_sample_dp_labels = family.get_subject_depth_labels()
+    # yield the resulting variants for this familiy
+    for row in gq:
+        
+        # interrogate the genotypes present in each family member to conforming
+        # to the genetic model being tested
+        gt_types = row['gt_types']
+        gts = row['gts']
+        gt_depths = row['gt_depths']
 
-        header = []
-        header.append("family_id")
-        for col in all_query_cols:
-            header.append(col)
-        for col in family_sample_gt_labels:
-            header.append(col)
-        for col in family_sample_dp_labels:
-            header.append(col)
-        yield header
+        # test the variant for each family in the db
+        for idx, fam_id in enumerate(family_ids):
+            family_genotype_mask = family_masks[idx]
+            family_sample_gt_label = family_sample_gt_labels[idx]
+            family_sample_gt_cols = family_sample_gt_columns[idx]
+            family_sample_dp_cols = family_sample_depth_columns[idx]
+            
 
-        # report the resulting de_novo variants for this familiy
-        for row in c:
-
-            # unpack the genotype arrays so that we can interrogate
-            # the genotypes present in each family member to conforming
-            # to the genetic model being tested
-            gt_types = compression.unpack_genotype_blob(row['gt_types'])
-            gt_depths = compression.unpack_genotype_blob(row['gt_depths'])
-            gts = compression.unpack_genotype_blob(row['gts'])
-
-            # does the variant meet the a de novo model for this family?
-            # if not, ignore.
+            # skip if the variant doesn't meet a de novo model
+            # for this family
             if not eval(family_genotype_mask):
                 continue
-
+                
             # make sure each sample's genotype had sufficient coverage.
             # otherwise, ignore
             insufficient_depth = False
-            for col in family_sample_depth_columns:
+            for col in family_sample_dp_cols:
                 depth = int(eval(col))
-                if depth < min_sample_depth:
+                if depth < args.min_sample_depth:
                     insufficient_depth = True
                     break
             if insufficient_depth:
                 continue
 
-            result = []
-            # first report all of the non-genotype columns
-            result.append(str(family.family_id))
-            for col in all_query_cols:
-                if col == 'gt_types' or col == 'gts':
-                    continue
-                result.append(str(row[col]))
-
-            # now report all of the genotype columns
-            for col in family_sample_gt_columns:
-                result.append(str(eval(col)))
-
-            # now report all of the depth columns
-            for col in family_sample_depth_columns:
-                result.append(str(eval(col)))
-
-            yield result
-
+            print str(fam_id) + "\t" + \
+               ",".join([str(s) for s in family_sample_gt_label]) + "\t", \
+               ",".join([str(eval(s)) for s in family_sample_gt_cols]) + "\t", \
+               ",".join([str(eval(s)) for s in family_sample_dp_cols]) + "\t",
+            print row
 
 def run(parser, args):
-
     if os.path.exists(args.db):
-        conn = sqlite3.connect(args.db)
-        conn.isolation_level = None
-        conn.row_factory = sqlite3.Row
-        c = conn.cursor()
+        get_de_novo_candidates_2(args)
 
-        for result in get_de_novo_candidates(c, args.min_sample_depth):
-            print '\t'.join(result)
