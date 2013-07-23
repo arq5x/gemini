5313da9d1296cefe3be961edc317711e02cc5b6a
arq5x <arq5x@virginia.edu>
2013-07-23 19:46:05 -0400

    [ENH] auto_dominant; add --filter, --columns, and --gt_filter.

--- gemini/tool_autosomal_dominant.py
+++ gemini/tool_autosomal_dominant.py
@@ -1,80 +1,77 @@
 #!/usr/bin/env python
 import sqlite3
 import os
-
-import compression
+import sys
+import GeminiQuery
 from gemini_constants import *
 import gemini_subjects as subjects
 
 
-def get_auto_dominant_candidates(c):
+def get_auto_dominant_candidates(args):
     """
     Report candidate variants that meet an autosomal dominant
     inheritance model.
     """
+    gq = GeminiQuery.GeminiQuery(args.db, include_gt_cols=True)
+    
+    if args.columns is not None:
+        # the user only wants to report a subset of the columns
+        query = "SELECT " + str(args.columns) + " FROM variants"
+    else:
+        # report the kitchen sink
+        query = "SELECT *" + \
+                ", gts, gt_types, gt_phases, gt_depths, \
+                gt_ref_depths, gt_alt_depths, gt_quals" + \
+                " FROM variants"
 
-    families = subjects.get_families(c)
+    # add any non-genotype column limits to the where clause
+    if args.filter:
+        query += " WHERE " + args.filter
 
+    # collect family info
+    families = subjects.get_families(gq.c)
+    family_ids = []
+    family_masks = []
+    family_sample_gt_labels = []
+    family_sample_gt_columns = []
     for family in families:
+        family_masks.append(family.get_auto_dominant_filter())
+        family_sample_gt_labels.append(family.get_subject_genotype_labels())
+        family_sample_gt_columns.append(family.get_subject_genotype_columns())
+        family_ids.append(family.family_id)
 
-        query = "SELECT chrom, start, end, ref, alt, gene, \
-                        impact, impact_severity, gt_types, gts \
-                 FROM variants \
-                 WHERE impact_severity != 'LOW'"
+    # run the query applying any genotype filters provided by the user.
+    gq.run(query, args.gt_filter)
 
-        c.execute(query)
-        all_query_cols = [str(tuple[0]) for tuple in c.description
-                          if not tuple[0].startswith("gt")]
+    # print a header
+    print "family_id\tfamily_members",
+    print gq.header
 
-        family_genotype_mask = family.get_auto_dominant_filter()
-        family_sample_gt_columns = family.get_subject_genotype_columns()
-        family_sample_gt_labels = family.get_subject_genotype_labels()
+    # yield the resulting variants for this familiy
+    for row in gq:
+        
+        # interrogate the genotypes present in each family member to conforming
+        # to the genetic model being tested
+        gt_types = row['gt_types']
+        gts = row['gts']
 
-        # yield a header
-        header = []
-        header.append("family_id")
-        for col in all_query_cols:
-            header.append(col)
-        for col in family_sample_gt_labels:
-            header.append(col)
-        yield header
-
-        # yield the resulting auto_dom variants for this familiy
-        for row in c:
-
-            # unpack the genotype arrays so that we can interrogate
-            # the genotypes present in each family member to conforming
-            # to the genetic model being tested
-            gt_types = compression.unpack_genotype_blob(row['gt_types'])
-            gts = compression.unpack_genotype_blob(row['gts'])
+        # test the variant for each family in the db
+        for idx, fam_id in enumerate(family_ids):
+            family_genotype_mask = family_masks[idx]
+            family_sample_gt_label = family_sample_gt_labels[idx]
+            family_sample_gt_cols = family_sample_gt_columns[idx]
 
             # skip if the variant doesn't meet a dominant model
             # for this family
+            
             if not eval(family_genotype_mask):
                 continue
 
-            result = []
-            # first report all of the non-genotype columns
-            result.append(str(family.family_id))
-            for col in all_query_cols:
-                if col == 'gt_types' or col == 'gts':
-                    continue
-                result.append(str(row[col]))
-
-            # now report all of the genotype columns
-            for col in family_sample_gt_columns:
-                result.append(str(eval(col)))
-
-            yield result
-
+            print str(fam_id) + "\t" + \
+               ",".join([str(s) for s in family_sample_gt_label]) + "\t", \
+               ",".join([str(eval(s)) for s in family_sample_gt_cols]) + "\t",
+            print row
 
 def run(parser, args):
-
     if os.path.exists(args.db):
-        conn = sqlite3.connect(args.db)
-        conn.isolation_level = None
-        conn.row_factory = sqlite3.Row
-        c = conn.cursor()
-
-        for result in get_auto_dominant_candidates(c):
-            print '\t'.join(result)
+        get_auto_dominant_candidates(args)
