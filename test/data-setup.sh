b62f04ce66eceb11ceaf419c164b966ee8b1c129
arq5x <arq5x@virginia.edu>
2013-08-22 21:12:38 -0400

    [TST] unit tests for auto_rec tool.

--- test/data-setup.sh
+++ test/data-setup.sh
@@ -10,3 +10,5 @@ gemini load -v test5.vep.snpeff.vcf -t VEP test5.vep.db
 gemini load -v test.query.vcf -t snpEff test.query.db
 gemini load -v test.region.vep.vcf -t VEP test.region.db
 gemini load -v test.burden.vcf -t VEP -p test.burden.ped test.burden.db
+gemini load -v test.auto_dom.vcf -t snpEff -p test.auto_dom.ped test.auto_dom.db
+gemini load -v test.auto_rec.vcf -t snpEff -p test.auto_rec.ped test.auto_rec.db
---
+++ test/test-auto-rec.sh
@@ -0,0 +1,59 @@
+###################################################################
+# 1. Test basic auto_recessive functionality
+###################################################################
+echo "    auto_rec.t1...\c"
+echo "family_id	family_members	family_genotypes	gene	chrom	start	end	ref	alt	impact	impact_severity
+2	2_dad(father; unaffected),2_mom(mother; unaffected),2_kid(child; affected)	C/T,C/T,T/T	ASAH2C	chr10	48004991	48004992	C	T	non_syn_coding	MED
+1	1_dad(father; unaffected),1_mom(mother; unaffected),1_kid(child; affected)	C/T,C/T,T/T	ASAH2C	chr10	48003991	48003992	C	T	non_syn_coding	MED
+3	3_dad(father; unaffected),3_mom(mother; unaffected),3_kid(child; affected)	T/C,T/C,C/C	SYCE1	chr10	135369531	135369532	T	C	non_syn_coding	MED
+1	1_dad(father; unaffected),1_mom(mother; unaffected),1_kid(child; affected)	T/C,T/C,C/C	WDR37	chr10	1142207	1142208	T	C	stop_loss	HIGH
+2	2_dad(father; unaffected),2_mom(mother; unaffected),2_kid(child; affected)	T/C,T/C,C/C	WDR37	chr10	1142207	1142208	T	C	stop_loss	HIGH" > exp
+gemini autosomal_recessive  \
+    --columns "gene, chrom, start, end, ref, alt, impact, impact_severity" \
+    test.auto_rec.db > obs
+check obs exp
+rm obs exp
+
+
+###################################################################
+# 2. Test with a minimum of 2 kindreds
+###################################################################
+echo "    auto_rec.t2...\c"
+echo "family_id	family_members	family_genotypes	gene	chrom	start	end	ref	alt	impact	impact_severity
+2	2_dad(father; unaffected),2_mom(mother; unaffected),2_kid(child; affected)	C/T,C/T,T/T	ASAH2C	chr10	48004991	48004992	C	T	non_syn_coding	MED
+1	1_dad(father; unaffected),1_mom(mother; unaffected),1_kid(child; affected)	C/T,C/T,T/T	ASAH2C	chr10	48003991	48003992	C	T	non_syn_coding	MED
+1	1_dad(father; unaffected),1_mom(mother; unaffected),1_kid(child; affected)	T/C,T/C,C/C	WDR37	chr10	1142207	1142208	T	C	stop_loss	HIGH
+2	2_dad(father; unaffected),2_mom(mother; unaffected),2_kid(child; affected)	T/C,T/C,C/C	WDR37	chr10	1142207	1142208	T	C	stop_loss	HIGH" > exp
+gemini autosomal_recessive  \
+    --columns "gene, chrom, start, end, ref, alt, impact, impact_severity" \
+    --min-kindreds 2 \
+    test.auto_rec.db > obs
+check obs exp
+rm obs exp
+
+###################################################################
+# 3. Test with a minimum of 3 kindreds
+###################################################################
+echo "    auto_rec.t3...\c"
+echo "family_id	family_members	family_genotypes	gene	chrom	start	end	ref	alt	impact	impact_severity" > exp
+gemini autosomal_recessive  \
+    --columns "gene, chrom, start, end, ref, alt, impact, impact_severity" \
+    --min-kindreds 3 \
+    test.auto_rec.db > obs
+check obs exp
+rm obs exp
+
+###################################################################
+# 4. Test with a filter and minimum of 2 kindreds
+###################################################################
+echo "    auto_rec.t4...\c"
+echo "family_id	family_members	family_genotypes	gene	chrom	start	end	ref	alt	impact	impact_severity
+1	1_dad(father; unaffected),1_mom(mother; unaffected),1_kid(child; affected)	T/C,T/C,C/C	WDR37	chr10	1142207	1142208	T	C	stop_loss	HIGH
+2	2_dad(father; unaffected),2_mom(mother; unaffected),2_kid(child; affected)	T/C,T/C,C/C	WDR37	chr10	1142207	1142208	T	C	stop_loss	HIGH" > exp
+gemini autosomal_recessive  \
+    --columns "gene, chrom, start, end, ref, alt, impact, impact_severity" \
+    --min-kindreds 2 \
+    --filter "impact_severity = 'HIGH'" \
+    test.auto_rec.db > obs
+check obs exp
+rm obs exp
