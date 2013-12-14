da397e4ebeab03736684ec62a5355d77f600dda3
arq5x <arq5x@virginia.edu>
2013-12-14 15:15:08 -0500

    [DOC] add script for creating VISTA enhancers annotation file.

---
+++ gemini/annotation_provenance/make-vista-enchancers.sh
@@ -0,0 +1,12 @@
+curl -s "http://enhancer.lbl.gov/cgi-bin/imagedb3.pl?show=1;page=1;form=ext_search;search.status=Positives;search.org=Human;page_size=100;search.result=yes;action=search;search.gene=;search.sequence=1" \
+  | grep "^>" \
+  > vista.human.positive.enhancers.headers.fa
+
+cat vista.human.positive.enhancers.headers.fa \
+  | tr -d "|" \
+  | perl -ne 's/\s+/\t/g; print "$_\n" ' \
+  | sed -e 's/\>Human//' \
+  | tr ":" "\t" \
+  | tr "-" "\t" \
+  | awk -v OFS="\t" '{list="positives:"; for(i=7; i<=NF; ++i) {list=list$i","}; print $1,$2,$3,$4"_"$5,list}' \
+  > hg19.vista.enhancers.20131108.bed
