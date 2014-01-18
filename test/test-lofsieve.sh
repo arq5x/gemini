ded8083fb563bd13106b41d84f55c499a861f3c2
udp3f <udp3f@d-172-26-6-250.bootp.virginia.edu>
2014-01-18 17:11:24 -0500

    unit test for lof-sieve

---
+++ test/test-lofsieve.sh
@@ -0,0 +1,11 @@
+################################################
+# Test the LOF sieve tool
+################################################
+echo "    sieve.t01...\c"
+echo "chrom	start	end	ref	alt	highest_impact	aa_change	var_trans_pos	trans_aa_length	var_trans_pct	sample	genotype	gene	transcript	trans_type
+chr1	1219381	1219382	C	G	stop_gain	S29*	29	138	0.210144927536	1719PC0012	C/G	SCNN1D	ENST00000470022	protein_coding" > obs
+
+gemini lof_sieve test.query.db > exp
+check obs exp
+rm obs exp
+################################################
