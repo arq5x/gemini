06ebe4b3914d211b0bd4ea9e1259fe66c8063bdc
udp3f <udp3f@eservices.virginia.edu>
2014-03-15 00:03:16 -0400

    new unit test added for testing lofsieve with VEP tool

--- test/test-lofsieve.sh
+++ test/test-lofsieve.sh
@@ -1,5 +1,5 @@
 ################################################
-# Test the LOF sieve tool
+# Test the LOF sieve tool with snpEff
 ################################################
 echo "    sieve.t01...\c"
 echo "chrom	start	end	ref	alt	highest_impact	aa_change	var_trans_pos	trans_aa_length	var_trans_pct	sample	genotype	gene	transcript	trans_type
@@ -9,3 +9,12 @@ gemini lof_sieve test.query.db > exp
 check obs exp
 rm obs exp
 ################################################
+# Test the LOF sieve tool with VEP
+################################################
+echo "    sieve.t02...\c"
+echo "chrom	start	end	ref	alt	highest_impact	aa_change	var_trans_pos	trans_aa_length	var_trans_pct	sample	genotype	gene	transcript	trans_type
+chr1	1219381	1219382	C	G	stop_gain	S/*	29	29/139	0.208633093525	1719PC0012	C/G	SCNN1D	ENST00000470022	None" > obs
+
+gemini lof_sieve test.query.vep.db > exp
+check obs exp
+rm obs exp
