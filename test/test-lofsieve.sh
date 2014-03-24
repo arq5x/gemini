ed7f05c6eb4ac0dd94097b7ac2be47d742a7491e
udp3f <udp3f@eservices.virginia.edu>
2014-03-23 23:02:57 -0400

    update biotype column

--- test/test-lofsieve.sh
+++ test/test-lofsieve.sh
@@ -13,7 +13,7 @@ rm obs exp
 ################################################
 echo "    sieve.t02...\c"
 echo "chrom	start	end	ref	alt	highest_impact	aa_change	var_trans_pos	trans_aa_length	var_trans_pct	sample	genotype	gene	transcript	trans_type
-chr1	1219381	1219382	C	G	stop_gain	S/*	29	29/139	0.208633093525	1719PC0012	C/G	SCNN1D	ENST00000470022	None" > obs
+chr1	1219381	1219382	C	G	stop_gain	S/*	29	29/139	0.208633093525	1719PC0012	C/G	SCNN1D	ENST00000470022	protein_coding" > obs
 
 gemini lof_sieve test.query.vep.db > exp
 check obs exp
