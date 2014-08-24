4e2ffe0cca09b4be8cf519b404682c6635ee934f
arq5x <arq5x@virginia.edu>
2014-08-24 15:57:23 -0400

    A basic tool for Mendelian violations.

--- gemini/gemini_main.py
+++ gemini/gemini_main.py
@@ -835,6 +835,37 @@ def main():
         tool_de_novo_mutations.run(parser, args)
     parser_de_novo.set_defaults(func=de_novo_fn)
 
+
+    #########################################
+    # $ gemini mendel violations
+    #########################################
+    parser_mendel = subparsers.add_parser('mendel_errors',
+            help='Identify candidate violations of Mendelian inheritance')
+    parser_mendel.add_argument('db',
+            metavar='db',
+            help='The name of the database to be queried.')
+    parser_mendel.add_argument('--columns',
+            dest='columns',
+            metavar='STRING',
+            help='A list of columns that you would like returned. Def. = "*"',
+            )
+    parser_mendel.add_argument('--filter',
+            dest='filter',
+            metavar='STRING',
+            help='Restrictions to apply to variants (SQL syntax)')
+    parser_mendel.add_argument('-d',
+            dest='min_sample_depth',
+            type=int,
+            help="The minimum aligned\
+                  sequence depth (genotype DP) req'd for\
+                  each sample (def. = 0)",
+            default=0)
+    def mendel_fn(parser, args):
+        import tool_mendel_errors
+        tool_mendel_errors.run(parser, args)
+    parser_mendel.set_defaults(func=mendel_fn)
+
+
     #########################################
     # $ gemini browser
     #########################################
