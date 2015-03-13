254e974055baef641cc584bf31f776ccb96c5f7f
Brent Pedersen (brentp) <bpederse@gmail.com>
2015-03-13 15:46:07 -0600

    update provenance scripts

--- gemini/annotation_provenance/make-clinvar.sh
+++ gemini/annotation_provenance/make-clinvar.sh
@@ -10,8 +10,10 @@
 #wget ftp://ftp.ncbi.nlm.nih.gov/pub/clinvar/vcf_GRCh37/clinvar_20140807.vcf.gz
 #wget ftp://ftp.ncbi.nlm.nih.gov/pub/clinvar/vcf_GRCh37/clinvar_20140807.vcf.gz.tbi
 
-wget -O - ftp://ftp.ncbi.nlm.nih.gov/pub/clinvar/vcf_GRCh37/clinvar_20150305.vcf.gz \
-	| vt decompose -s | vt normalize -r /data/human/b37/human_g1k_v37_decoy.fasta - \
+wget ftp://ftp.ncbi.nlm.nih.gov/pub/clinvar/vcf_GRCh37/clinvar_20150305.vcf.gz
+wget ftp://ftp.ncbi.nlm.nih.gov/pub/clinvar/vcf_GRCh37/clinvar_20150305.vcf.gz.tbi
+
+vt decompose -s clinvar_20150305.vcf.gz | vt normalize -r /data/human/b37/human_g1k_v37_decoy.fasta - \
 	| bgzip -c > clinvar_20150305.tidy.vcf.gz
 tabix clinvar_20150305.tidy.vcf.gz
 
