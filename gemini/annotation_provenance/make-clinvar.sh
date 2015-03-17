36fc216eba43a5b7bed20e65ccf81ee9a10c030d
Brent Pedersen (brentp) <bpederse@gmail.com>
2015-03-17 16:44:12 -0600

    update clinvar script to adjust header to be Number=A for CLN*

--- gemini/annotation_provenance/make-clinvar.sh
+++ gemini/annotation_provenance/make-clinvar.sh
@@ -13,7 +13,13 @@
 wget ftp://ftp.ncbi.nlm.nih.gov/pub/clinvar/vcf_GRCh37/clinvar_20150305.vcf.gz
 wget ftp://ftp.ncbi.nlm.nih.gov/pub/clinvar/vcf_GRCh37/clinvar_20150305.vcf.gz.tbi
 
-vt decompose -s clinvar_20150305.vcf.gz | vt normalize -r /data/human/b37/human_g1k_v37_decoy.fasta - \
-	| bgzip -c > clinvar_20150305.tidy.vcf.gz
-tabix clinvar_20150305.tidy.vcf.gz
+VCF=clinvar_20150305.vcf.gz
+zless $VCF | perl -pe 's/##INFO=<ID=CLN(DBN|ACC|SRCID|SIG|DADB|SRC|ORIGIN|ALLE|HGVS|DSDBID|DSDBD|DSDB),Number=\./##INFO=<ID=CLN\1,Number=A/' | bgzip -c > tmp.vcf.gz
 
+tabix tmp.vcf.gz
+
+vt decompose -s tmp.vcf.gz \
+		   | vt normalize -r /data/human/b37/human_g1k_v37_decoy.fasta - \
+		   | bgzip -c > clinvar_20150305.tidy.vcf.gz
+tabix clinvar_20150305.tidy.vcf.gz
+rm tmp.vcf.gz
