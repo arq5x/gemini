a607d772b448bd09fa222b927adea71d2dc5ddb2
Brent Pedersen <bpederse@gmail.com>
2015-03-26 16:32:27 -0600

    sneak in bp version

--- gemini/scripts/gemini_install.py
+++ gemini/scripts/gemini_install.py
@@ -37,6 +37,13 @@ remotes_dev.update({
     "requirements_conda": "https://raw.githubusercontent.com/arq5x/gemini/dev/versioning/unstable/requirements_conda.txt",
 })
 
+remotes_bp = remotes_dev
+remotes_bp.update({
+    "requirements_pip": "https://raw.github.com/brentp/gemini/dev/requirements.txt",
+    "gemini": "git+https://github.com/brentp/gemini.git@dev",
+    "requirements_conda": "https://raw.githubusercontent.com/brentp/gemini/dev/versioning/unstable/requirements_conda.txt",
+})
+
 
 def main(args):
     check_dependencies()
@@ -45,8 +52,12 @@ def main(args):
         os.makedirs(work_dir)
     os.chdir(work_dir)
 
-    if args.gemini_version == "unstable":
-        remotes = remotes_dev
+    if args.gemini_version in ("unstable", "bp"):
+        if args.gemini_version == "unstable":
+            remotes = remotes_dev
+        else:
+            remotes = remotes_bp
+
         requirements_pip = remotes['requirements_pip']
         requirements_conda = remotes['requirements_conda']
         urllib.urlretrieve(requirements_pip, filename='_pip_dev.txt')
