d3bdca5a8cf18dd69055682ec857b04e913c17f6
Brent Pedersen (brentp) <bpederse@gmail.com>
2015-05-12 12:54:58 -0600

    more tests for bcolz.

--- test/bcolz-test.sh
+++ test/bcolz-test.sh
@@ -8,6 +8,7 @@ check()
 }
 export -f check
 
-for f in test*.sh; do
+#for f in test*.sh; do
+for f in test-query.sh test-genotypes.sh test-genotype-likelihoods.sh; do
 	sed 's/gemini query/gemini query --use-bcolz/' $f | bash
 done
