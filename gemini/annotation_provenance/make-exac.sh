3af5594c0dbe4b91f16cb7a7f2bc087d11961835
Brent Pedersen (brentp) <bpederse@gmail.com>
2015-07-15 10:12:43 -0600

    update ExAC processing to use smart (-s) decomposition. fixes bug in ExAC AF that was reporting on the mailing list.

--- gemini/annotation_provenance/make-exac.sh
+++ gemini/annotation_provenance/make-exac.sh
@@ -1,5 +1,5 @@
-# v0.12
+# v0.16.3
 wget -O - ftp://ftp.broadinstitute.org/pub/ExAC_release/release0.3/ExAC.r0.3.sites.vep.vcf.gz \
-	| vt decompose - | vt normalize -r /data/human/b37/human_g1k_v37_decoy.fasta - \
+	| vt decompose -s - | vt normalize -r /data/human/b37/human_g1k_v37_decoy.fasta - \
 	| bgzip -c > ExAC.r0.3.sites.vep.tidy.vcf.gz
 tabix ExAC.r0.3.sites.vep.tidy.vcf.gz
