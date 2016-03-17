83252a75c8eb53a9498dbf3b56dbc0515f3c3d4d
Brent Pedersen (brentp) <bpederse@gmail.com>
2016-03-17 12:26:47 -0600

    update requirements and test to include sqlalchemy

--- test/test-multi-col.sh
+++ test/test-multi-col.sh
@@ -3,7 +3,7 @@ echo "test.multi-col.t1"
 
 echo "variant_id	chrom	start	ref	alt	gene	biotype	gene	variant_id	biotype	transcript
 1	chr1	30547	T	G	FAM138A	protein_coding	FAM138A	1	protein_coding	ENST00000417324
-1	chr1	30547	T	G	FAM138A	protein_coding	FAM138A	1	protein_coding	ENST00000461467" > exp
+1	chr1	30547	T	G	FAM138A	processed_transcript	FAM138A	1	processed_transcript	ENST00000461467" > exp
 
 gemini query --header -q "select v.variant_id, v.chrom, v.start, v.ref, v.alt, v.gene, v.biotype, i.gene, i.variant_id, i.biotype, i.transcript from variants\
                           v, variant_impacts i where v.variant_id=i.variant_id and v.variant_id = 1 and i.gene = 'FAM138A' order by i.anno_id" test.query.db > obs
