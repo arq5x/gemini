7534dbd0c50fbc57e737f0c39d7a899bd97f6f38
Brent Pedersen (brentp) <bpederse@gmail.com>
2016-03-17 11:24:33 -0600

    order

--- test/test-multi-col.sh
+++ test/test-multi-col.sh
@@ -6,5 +6,5 @@ echo "variant_id	chrom	start	ref	alt	gene	biotype	gene	variant_id	biotype	transc
 1	chr1	30547	T	G	FAM138A	protein_coding	FAM138A	1	protein_coding	ENST00000461467" > exp
 
 gemini query --header -q "select v.variant_id, v.chrom, v.start, v.ref, v.alt, v.gene, v.biotype, i.gene, i.variant_id, i.biotype, i.transcript from variants\
-                          v, variant_impacts i where v.variant_id=i.variant_id limit 2" test.query.db > obs
+                          v, variant_impacts i where v.variant_id=i.variant_id and v.variant_id = 1 and i.gene = 'FAM138A' order by i.anno_id" test.query.db > obs
 check obs exp
